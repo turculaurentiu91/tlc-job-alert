3f16f2aa41232995472d1ade8725bc15ef6c7d91
Turcu Laurentiu <turculaurentiu91@gmail.com>
2019-01-04 23:01:27 +0100

    Added tab-button component

--- assets/js/admin-menu.js
+++ assets/js/admin-menu.js
@@ -2,15 +2,47 @@ Vue.use(Vuex);
 
 const store = new Vuex.Store({
   state: {
-    activePage: ''
+    activePage: 'subscriptions',
   },
-  mutations: {},
+  getters: {
+    activePage: state => state.activePage,
+  },
+  mutations: {
+    setPage: (state, page) => state.activePage = page,
+  },
+});
+
+Vue.component('tab-button', {
+  template: '#tab-button-template',
+  props: {
+    pageSlug: {
+      type: String,
+      required: true,
+    },
+  },
+  computed: {
+    isActive() { return this.$store.state.activePage == this.pageSlug; },
+  },
+  methods: {
+    ...Vuex.mapMutations([
+      'setPage',
+    ]),
+  },
 });
 
 const adminApp = new Vue({
   el: "#admin-app",
   store,
   data: {},
-  computed: {},
-  methods: {},
+  computed: {
+    ...Vuex.mapGetters([
+      'activePage',
+    ]),
+  },
+  methods: {
+    ...Vuex.mapMutations([
+      'setPage'
+    ]),
+  },
 });
+
--- src/templates/admin-menu.php
+++ src/templates/admin-menu.php
@@ -2,7 +2,22 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/vuex/3.0.1/vuex.js"></script>
 
 <div class="wrap" id="admin-app">
-  
+  <h2 class="nav-tab-wrapper">
+    <tab-button page-slug="subscriptions">
+      <?= __("Subscriptions", "tlc-job-alert") ?>
+    </tab-button>
+    <tab-button page-slug="email-template">
+      <?= __("E-mail Templates", "tlc-job-alert") ?>
+    </tab-button>
+  </h2>
+
+  <script type="text/x-template" id="tab-button-template">
+    <a href="#" class="nav-tab" v-bind:class="{'nav-tab-active': isActive}"
+      @click.prevent="setPage(pageSlug); $event.target.blur()"
+      >
+      <slot></slot>
+    </a>
+  </script>
 </div>
 
 <script src="<?= plugins_url('assets/js/admin-menu.js', TLC_JOB_ALERT_PLUGIN_DIR) ?>"></script>
