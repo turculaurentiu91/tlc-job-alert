d188c691e932a9692e5ced3dff54035342675c81
Turcu Laurentiu <turculaurentiu91@gmail.com>
2019-01-04 19:54:43 +0100

    initial Commit

---
+++ tlc-job-alert.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name: TLC Job Alert
+Plugin URI: https://www.fiverr.com/laurentiuturcu
+Author URI: https://www.fiverr.com/laurentiuturcu
+Description: Alerts for WP Job Manager
+Version: 1.0.0
+Author: Turcu Laurentiu
+Text Domain: tlc-job-alerts
+*/
+
+require 'vendor/autoload.php';
