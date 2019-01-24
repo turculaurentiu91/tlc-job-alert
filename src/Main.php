6a97f9ee3bbc7f86f4f0a14a86db337763c138b3
Turcu Laurentiu <turculaurentiu91@gmail.com>
2019-01-24 19:08:42 +0100

    added a redbean connection setup call in main constuctor

--- src/Main.php
+++ src/Main.php
@@ -1,10 +1,18 @@
 <?php namespace TlcJobAlert;
 
+use \RedBeanPHP\R as R;
+
 class Main {
   private $adminPage;
   private $shortcodes;
 
   public function __construct() {
+    R::setup(
+      "mysql:host=" . DB_HOST . ";" . "dbname=" . DB_NAME,
+      DB_USER,
+      DB_PASSWORD
+    );
+    
     $this->adminPage = new AdminPage();
     $this->shortcodes = new Shortcodes();
   }
