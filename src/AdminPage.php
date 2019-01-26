815949caad6a9db5ade86ddcb331a87c6876369e
Turcu Laurentiu <turculaurentiu91@gmail.com>
2019-01-26 19:26:13 +0100

    Made a method to load the scripts wordpress way

--- src/AdminPage.php
+++ src/AdminPage.php
@@ -4,6 +4,7 @@ class AdminPage {
 
   public function __construct() {
     add_action('admin_menu', array($this, 'add_submenu'));
+    add_action('admin_enqueue_scripts', array($this, 'enqueue_assets'));
   }
 
   public function add_submenu() {
@@ -12,9 +13,41 @@ class AdminPage {
       __("TLC Job Alert", "tlc-job-alert"),
       __("TLC Job Alert", "tlc-job-alert"),
       'manage_options',
-      'tlc-job-ealert-admin',
+      'tlc-job-alert-admin',
       array($this, 'render_admin_page')
     );
+  }
+
+  public function enqueue_styles() {
+
+  }
+
+  public function enqueue_assets() {
+    if (isset($_GET['page'])) {
+      if ($_GET['page'] == 'tlc-job-alert-admin') {
+        wp_enqueue_script(
+          'vuejs',
+          'https://cdn.jsdelivr.net/npm/vue@2.5.17/dist/vue.js',
+          array(),
+          '2.5.17'
+        );
+    
+        wp_enqueue_script(
+          'vuex',
+          'https://cdnjs.cloudflare.com/ajax/libs/vuex/3.0.1/vuex.js',
+          array(),
+          '3.0.1'
+        );
+
+        wp_enqueue_script(
+          'job-alert-admin',
+          TLC_JOB_ALERT_PATH_URL . 'assets/js/admin-menu.js',
+          array(),
+          '1.0.0',
+          true
+        );
+      }
+    }
   }
 
   public function render_admin_page()
