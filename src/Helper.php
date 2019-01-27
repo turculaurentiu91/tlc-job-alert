9a56593764ca093260304127028f65f14476a614
Turcu Laurentiu <turculaurentiu91@gmail.com>
2019-01-27 18:25:17 +0100

    Added data to the template method and a method to generate random strings

--- src/Helper.php
+++ src/Helper.php
@@ -1,11 +1,24 @@
 <?php namespace TlcJobAlert;
 
 class Helper {
-  public static function getTemplate($file) {
+  public static function getTemplate($file, $data = array()) {
+    extract($data);
     ob_start();
     require(TLC_JOB_ALERT_PATH_DIR . "src/templates/{$file}.php");
     $content = ob_get_contents();
     ob_end_clean();
     return $content;
+  }
+
+  public static function randString($length = 10)
+  {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomString = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomString .= $characters[rand(0, $charactersLength - 1)];
+    }
+    $randomString[0] = "_";
+    return $randomString;
   }
 }
