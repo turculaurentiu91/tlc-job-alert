db23c0b346ff72600c31979d6eaa8095411cd62f
Turcu Laurentiu <turculaurentiu91@gmail.com>
2019-01-28 21:46:02 +0100

    Added some data to the job notification email

--- src/templates/newJobEmail.php
+++ src/templates/newJobEmail.php
@@ -1,1 +1,11 @@
-<h2>NEW JOB <?= $jobID ?> </h2>
+<h2>There is a new job that meets your filters</h2>
+
+<a href="<?= get_permalink($jobID) ?>">Go to job listing </a>
+
+<p><?php 
+  setup_postdata($jobID);
+  echo get_the_excerpt($jobID);
+?></p>
+
+<p>If you don't want to recieve those emails anymore, click this
+<a href="<?= get_permalink($jobID) ?>">unsubscribe</a> link.
